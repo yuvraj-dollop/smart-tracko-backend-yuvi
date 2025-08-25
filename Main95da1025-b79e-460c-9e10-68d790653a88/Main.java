66ca39d913b4b89b2d72bd5c7cf3244e2659746f
Raj <rajdubliya@gmail.com>
2025-08-25 18:44:59 +0530

    update code till date: 25-08-2025

---
+++ Main95da1025-b79e-460c-9e10-68d790653a88/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+ class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
--- src/main/java/com/cico/controller/CompilerController.java
+++ src/main/java/com/cico/controller/CompilerController.java
@@ -1,5 +1,10 @@
 package com.cico.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,25 +12,20 @@ import com.cico.payload.CompilerRequest;
 import com.cico.payload.CompilerResponse;
 import com.cico.service.ICompilerService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 @RestController
 @RequestMapping("/compiler")
 public class CompilerController {
 
-	
 	@Autowired
 	private ICompilerService compilerService;
-	
-	
+
 	@PostMapping("/compile")
 	public ResponseEntity<CompilerResponse> compileCode(@RequestBody CompilerRequest request) {
-		return new ResponseEntity<>(compilerService.compileCode(request),HttpStatus.OK);
+		return new ResponseEntity<>(compilerService.compileCode(request), HttpStatus.OK);
 	}
-	
+
+	@PostMapping("/v2/compile")
+	public ResponseEntity<CompilerResponse> compileCodeNew(@RequestBody CompilerRequest request) {
+		return new ResponseEntity<>(compilerService.compileCode(request), HttpStatus.OK);
+	}
 }
