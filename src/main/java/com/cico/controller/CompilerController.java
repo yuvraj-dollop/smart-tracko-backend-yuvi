90a8eb94247dc6cded789f752d8bcf26310fc57f
Raj <rajdubliya@gmail.com>
2025-08-26 14:11:10 +0530

    update code till date: 26-08-2025

--- src/main/java/com/cico/controller/CompilerController.java
+++ src/main/java/com/cico/controller/CompilerController.java
@@ -1,8 +1,12 @@
 package com.cico.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +32,15 @@ public class CompilerController {
 	public ResponseEntity<CompilerResponse> compileCodeNew(@RequestBody CompilerRequest request) {
 		return new ResponseEntity<>(compilerService.compileCode(request), HttpStatus.OK);
 	}
+
+	@PostMapping("/v2/execute")
+	public ResponseEntity<CompilerResponse> execute(@RequestBody CompilerRequest request) {
+		return ResponseEntity.ok(compilerService.execute(request));
+	}
+
+	@GetMapping("/v2/runtimes")
+	public ResponseEntity<List<Map<String, Object>>> getRuntimes() {
+		return ResponseEntity.ok(compilerService.getRuntimes());
+	}
+
 }
